e0007f20de6336506ef6aac5b7605132800e7d97
trithepChaliewtham <trithep.ch.62@ubu.ac.th>
2020-08-07 07:07:35 +0000

    เพิ่ม / บรรทัดใน gitignore

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l#d)=k3^2f9^39&__hb26y@cf+p95jv#g=p67-5yf9a1gi-$1+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [u'wichit2s.pythonanywhere.com']
+ALLOWED_HOSTS = [u'trithep.pythonanywhere.com']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myweb',
+
 ]
 
 MIDDLEWARE = [
