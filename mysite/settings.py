8921dab798cedb5a36225c21b4a66caf182d696c
unknown <trithep.ch.62@ubu.ac.th>
2020-09-21 17:54:32 +0700

    Edit error database

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
+import os
 
-from pathlib import Path
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -29,7 +29,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = [u'localhost',u'127.0.0.1',u'trithep.pythonanywhere.com']
 
+STATICFILES_DIRS = [
 
+    os.path.join(BASE_DIR, "static"),
+
+]
 
 # Application definition
 
@@ -81,7 +85,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -124,18 +128,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-
+STATIC_URL = '/static/'
 
 # default static files settings for PythonAnywhere.
 # see https://help.pythonanywhere.com/pages/DjangoStaticFiles for more info
 MEDIA_ROOT = u'/home/trithep/mysite/media'
 MEDIA_URL = '/media/'
-
-STATICFILES_DIRS = [
-
-    BASE_DIR / "static",
-
-]
-
-
-STATIC_URL = '/static/'
